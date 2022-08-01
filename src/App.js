b0d150642b8346d106b2daf0ab65c67ca35ee8b3
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-01 02:30:38 -0400

    Added some useEffect hook examples fetching data once component is rendered etc.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import StarRating from './components/StarRating';
+import { click } from '@testing-library/user-event/dist/click';
+import { FaSpinner } from 'react-icons/fa';
+
+import { CgSpinnerAlt } from 'react-icons/cg';
 
 const UseStateComponent01 = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`Count is ${count}`);
+  }
+  , [count]); // Only run the effect if count changes
+
   return (
     <div>
       <p>You clicked {count} times</p>
@@ -45,12 +55,73 @@ const UseStateComponent03 = () => {
   );
 }
 
+const UseEffectComponent01 = () => {
+  const [name, setName] = useState('Alan');
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    console.log(`Celebrate ${name}`);
+    
+  }, [name]); // Only run the effect if name changes
+
+  useEffect(() => {
+    console.log(`${name} is ${admin ? 'admin' : 'not admin'}`);
+    document.title = `${name} is ${admin ? 'admin' : 'not admin'}`; // Update the document title
+  }, [admin]); // Only run the effect if admin changes
+
+  return (
+    <section>
+      <p>Congratulations {name}</p>
+      <button onClick={() => setName('Max')}>
+        Change Winner
+      </button>
+      <p>{admin ? <span>logged in</span> : <span>not logged in</span>}</p>
+      {admin && <button onClick={() => setAdmin(false)}>Logout</button>}
+      {!admin && <button onClick={() => setAdmin(true)}>Login</button>}
+    </section>
+  )
+}
+
+
+const UseEffectFetchData = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setTimeout(
+      async () => {
+        await fetch('https://api.github.com/users')
+          .then(response => response.json())
+          .then(json => setData(json));
+      }, 3750);
+  }
+  , []); // Only run the effect once, on mount
+
+  return (
+    <div>
+      <h2>Github Users</h2>
+      {/* <button onClick={() => setData(null)} >
+        {data ? 'Reset' : '(Github users are permanently gone until page is refreshed!)'}
+      </button> */}
+      {data ? data.map((user, key) => (
+        <div key={key}>
+          <img width={128} height={128} src={user.avatar_url} alt={user.login} />
+          <p>{user.login}</p>
+        </div>
+    )) : <span ><CgSpinnerAlt className='my-spinner' /></span>}
+    </div>
+  );
+}
+
 
 
 
 const App = (props) => {
   return (
     <div className="App">
+      <UseEffectFetchData />
+
+      <UseEffectComponent01 />
+
       <StarRating totalStars={10} />
       <UseStateComponent01 />
       <UseStateComponent02 />
