6a20af8cb0276a1af46f1f566a7255f8dd42b0d4
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-03 15:45:54 -0400

    Final version of the Main app for experimenting with React.js concepts

--- src/App.js
+++ src/App.js
@@ -1,131 +1,57 @@
 import './App.css';
 
-import { useState, useEffect } from 'react';
-
 import StarRating from './components/StarRating';
-import { click } from '@testing-library/user-event/dist/click';
-import { FaSpinner } from 'react-icons/fa';
+import FormWithCustomHook from './components/FromWithCustomHook';
 
-import { CgSpinnerAlt } from 'react-icons/cg';
-
-const UseStateComponent01 = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`Count is ${count}`);
-  }
-  , [count]); // Only run the effect if count changes
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
-
-const UseStateComponent02 = () => {
-  const [status, setStatus] = useState('Not Delivered');
-
-  return (
-    <div>
-      <p>Status: {status}</p>
-      <button onClick={() => {
-        status === 'Not Delivered' ? setStatus('Delivered') : setStatus('Not Delivered');
-        }}>
-        Toggle Status
-      </button>
-    </div>
-  );
-}
-
-const UseStateComponent03 = () => {
-  const [check, setCheck] = useState(false);
-
-  return (
-    <div>
-      <p>Check: {check ? 'Checked' : 'Not Checked'}</p>
-      <label>
-        <input type="checkbox" checked={check} onChange={() => setCheck(!check)} />
-        Checkbox
-      </label>
-    </div>
-  );
-}
-
-const UseEffectComponent01 = () => {
-  const [name, setName] = useState('Alan');
-  const [admin, setAdmin] = useState(false);
-
-  useEffect(() => {
-    console.log(`Celebrate ${name}`);
-    
-  }, [name]); // Only run the effect if name changes
-
-  useEffect(() => {
-    console.log(`${name} is ${admin ? 'admin' : 'not admin'}`);
-    document.title = `${name} is ${admin ? 'admin' : 'not admin'}`; // Update the document title
-  }, [admin]); // Only run the effect if admin changes
-
-  return (
-    <section>
-      <p>Congratulations {name}</p>
-      <button onClick={() => setName('Max')}>
-        Change Winner
-      </button>
-      <p>{admin ? <span>logged in</span> : <span>not logged in</span>}</p>
-      {admin && <button onClick={() => setAdmin(false)}>Logout</button>}
-      {!admin && <button onClick={() => setAdmin(true)}>Login</button>}
-    </section>
-  )
-}
-
-
-const UseEffectFetchData = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    setTimeout(
-      async () => {
-        await fetch('https://api.github.com/users')
-          .then(response => response.json())
-          .then(json => setData(json));
-      }, 3750);
-  }
-  , []); // Only run the effect once, on mount
-
-  return (
-    <div>
-      <h2>Github Users</h2>
-      {/* <button onClick={() => setData(null)} >
-        {data ? 'Reset' : '(Github users are permanently gone until page is refreshed!)'}
-      </button> */}
-      {data ? data.map((user, key) => (
-        <div key={key}>
-          <img width={128} height={128} src={user.avatar_url} alt={user.login} />
-          <p>{user.login}</p>
-        </div>
-    )) : <span ><CgSpinnerAlt className='my-spinner' /></span>}
-    </div>
-  );
-}
-
-
+import UseStateComponent01 from './components/UseStateComponent01';
+import UseStateComponent02 from './components/UseStateComponent02';
+import UseStateComponent03 from './components/UseStateComponent03';
+import ControlledComponentExample from './components/ControlledComponentExample';
+import UseRefExample from './components/UseRefExample';
+import UseReducerDispatch from './components/UseReducerDispatch';
+import UseReducerComponent from './components/UseReducerComponent';
+import UseEffectFetchData from './components/UseEffectFetchData';
+import UseEffectComponent01 from './components/UseEffectComponent01';
+import CheckBoxWithUseReducer from './components/CheckboxWithUseReducer';
+import UseContextExample from './components/UseContextExample';
+import FetchDataWithCustomHook from './components/FetchDataWithCustomHook';
 
 
 const App = (props) => {
   return (
     <div className="App">
+      <FetchDataWithCustomHook />
+      <hr/>
+      <UseContextExample />
+      <h3>Form with Custom Hook (useInput)</h3>
+      <FormWithCustomHook />
+      <hr/>
+      <h3>Form with Controlled Component (useState)</h3>
+      <ControlledComponentExample />
+      <hr/>
+      <h3>Form using useRef</h3>
+      <UseRefExample />
+      <hr/>
+      <UseReducerDispatch />
+      <hr/>
+      <CheckBoxWithUseReducer />
+      <hr/>
+      <UseReducerComponent />
+      <hr/>
       <UseEffectFetchData />
+      <hr/>
 
       <UseEffectComponent01 />
+      <hr/>
 
       <StarRating totalStars={10} />
+      <hr/>
       <UseStateComponent01 />
+      <hr/>
       <UseStateComponent02 />
+      <hr/>
       <UseStateComponent03 />
+      <hr/>
       <h2>Number: {props.number}</h2>
       {props.people.map((person, key) => (
         <div key={key + person.id}>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -13,11 +14,40 @@ const people = [
   { id: Date.now() + Math.ceil(Math.random() * 9999), name: 'Stephanie', age: 26 }
 ];
 
+export const TreesContext = createContext();
+
+export const useTrees = () => {
+  return useContext(TreesContext);
+}
+
+const trees = [
+  {
+    id: 1,
+    name: 'Oak',
+    color: 'green',
+    age: 10,
+  },
+  {
+    id: 2,
+    name: 'Pine',
+    color: 'green',
+    age: 20,
+  },
+  {
+    id: 3,
+    name: 'Cherry',
+    color: 'red',
+    age: 30,
+  }
+];
+
 console.log(people);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App people={people} number={third} />
+    <TreesContext.Provider value={{ trees }}>
+      <App people={people} number={third} />
+    </TreesContext.Provider>
   </React.StrictMode>
 );
